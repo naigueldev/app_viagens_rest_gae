706e7c367a8f317992bcd6b0ee5cd9988f84675c
naigueldev <naiguelsantosdeveloper@gmail.com>
2017-04-18 20:07:08 -0300

    config web xml

--- src/com/estudo/nosql/gae/InsereCidade.java
+++ src/com/estudo/nosql/gae/InsereCidade.java
@@ -1,8 +1,17 @@
 package com.estudo.nosql.gae;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 public class InsereCidade extends HttpServlet{
-
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().write("Ok, vamos inserir!");
+	}
 }
